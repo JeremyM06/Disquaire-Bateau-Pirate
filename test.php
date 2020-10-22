0b8380927c287592a955be155de64357cef6c668
Jeremy Martin <jeremymartin06@gmail.com>
2020-10-22 08:55:17 +0200

    base de données

--- test.php
+++ test.php
@@ -1,9 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bateau-pirate</title>
+    <link rel="stylesheet" href="style/style.css">
+
+</head>
+<body>
 <?php
 $connexion='mysql:host=localhost;dbname=disquaire';
 $user="web001";
 $pass="topsecret";
-
-
 try{
     $dbd = new PDO($connexion, $user, $pass);
     $dbd->query('SET NAMES UTF8');// pour afficher les nom correctement sans erreur d'accent etc.
@@ -12,10 +20,18 @@ try{
     print "Grosse catastrophe!:".$e->getMessage()."<br>";
     die();
 }
+?>
+<div class="cardsBlock">
+<?php
 
-
-foreach($dbd->query("SELECT * FROM disques")as $values){
-    echo 'Voici un commentaire: <em>'.$values['id_artiste'].'</em> dans l\'album '. $values['album'];
-}
+    foreach($dbd->query("SELECT D.image, A.nom, D.album, C.categorie FROM disques D, artistes A, categories C WHERE A.id = D.artiste AND C.id = D.categorie")as $values){
+        echo '<div class="cards"><img src="assets/images/'.$values['image'].'" alt=""><br>'.$values['nom']." est l'interprete de l'album ".$values['album']." et de la categorie ".$values['categorie'].'</div>';
+}?>
+</div>
+<?php
 
 ?>
+
+</body>
+</html>
+
