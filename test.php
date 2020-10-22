177a63bee3be8df1a24747e5acc5844621b5431c
Jeremy Martin <jeremymartin06@gmail.com>
2020-10-22 10:39:29 +0200

    structure

--- test.php
+++ test.php
@@ -9,23 +9,16 @@
 </head>
 <body>
 <?php
-$connexion='mysql:host=localhost;dbname=disquaire';
-$user="web001";
-$pass="topsecret";
-try{
-    $dbd = new PDO($connexion, $user, $pass);
-    $dbd->query('SET NAMES UTF8');// pour afficher les nom correctement sans erreur d'accent etc.
-
-}catch (PDOException $e){
-    print "Grosse catastrophe!:".$e->getMessage()."<br>";
-    die();
-}
+include "inc/connexion.php";
 ?>
 <div class="cardsBlock">
 <?php
 
-    foreach($dbd->query("SELECT D.image, A.nom, D.album, C.categorie FROM disques D, artistes A, categories C WHERE A.id = D.artiste AND C.id = D.categorie")as $values){
+    foreach($dbd->query("SELECT D.image, A.nom, D.album, C.categorie FROM disques D, artistes A, categories C WHERE A.id = D.artiste AND C.id = D.categorie ORDER BY D.id DESC LIMIT 0,5 ")as $values){
         echo '<div class="cards"><img src="assets/images/'.$values['image'].'" alt=""><br>'.$values['nom']." est l'interprete de l'album ".$values['album']." et de la categorie ".$values['categorie'].'</div>';
+        echo "<pre>";
+        print_r($values);
+        echo "</pre>";
 }?>
 </div>
 <?php
