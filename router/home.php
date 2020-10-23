73103c4e3af07bc6b280b93ee54a4993aeea8e25
Jeremy Martin <jeremymartin06@gmail.com>
2020-10-23 15:43:45 +0200

    reglage erreur merge

--- router/home.php
+++ router/home.php
@@ -8,10 +8,6 @@ $contenu .='<div class="container-fluid">
             <p><i>Aujourd\'hui sur Bateau-Pirate, 631 Cd Promo vous attendent au sein de notre rayon . Et cela, aussi
                     bien du
                     côté du neuf que des produits Bateau-Pirate occasion.</i></p>
-<<<<<<< HEAD
-=======
-            <a class="btn btn-primary btn-warning" href="index.php?page=catalogue" role="button">Reserver un disque!</a>
->>>>>>> 5aebf8398317b414d6f7d7e6787b62f5a1b6de55
         </div>
     </div>';
 
